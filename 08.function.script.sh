449211024571900bd427b2426cc7f248814028a0
sai <spyadala91@gmail.com>
2025-02-05 22:54:48 +0000

    add conversation

---
+++ 08.function.script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE{}{
+if [ $1 -ne 0 ]
+then
+   echo "$2 ..failure"
+   exit 1
+   else
+   echo "$2..sucess"
+ fi   
+}
+ 
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR:: you must have sudo access to execute this script"
+   exit 1 #other than 0
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+dnf install mysql -y
+VALIDATE $? "installing Mysql"
+
+else
+   echo "Mysql is already installed"
+fi       
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+   dnf install git -y
+   VALIDATE $? "installing git"
+else
+   echo "git is already..installed"
+fi       
