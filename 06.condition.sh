ad7b2390271a2382e204ef40fc4a7a26bbf3b1d7
sai <spyadala91@gmail.com>
2025-02-05 09:45:56 +0000

    add conversation

--- 06.condition.sh
+++ 06.condition.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
-echo " given number is greater than 100 "
+    echo "given number is greater than 100"
 else
-echo " given number is less than or equal to 100 "
+    echo "given number is less than or equal to 100"
 fi
